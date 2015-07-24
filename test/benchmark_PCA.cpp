21e4905f69bf1b5274f2a581aefeada93eb3563a
riku <riku@mdl.cs.tsukuba.ac.jp>
2015-07-24 13:38:14 +0900

    print PCA timing

--- test/benchmark_PCA.cpp
+++ test/benchmark_PCA.cpp
@@ -14,55 +14,73 @@
     } while (0)
 
 int main(int argc, char *argv[]) {
-    long m, p, r, L;
+    long m, p, r, L, f;
     ArgMapping argmap;
     MDL::Timer keyTimer, totalTimer;
-
+	std::string file;
     argmap.arg("m", m, "m");
     argmap.arg("L", L, "L");
     argmap.arg("p", p, "p");
     argmap.arg("r", r, "r");
+    argmap.arg("f", f, "file");
     argmap.parse(argc, argv);
-    MDL::Matrix<long> mat = load_csv("covariance3.data");
+
+	switch (f) {
+	case 0: file = "covariance.data"; break;
+	case 1: file = "covariance2.data"; break;
+	case 2: file = "covariance3.data"; break;
+	default: file = "covariance.data"; break;
+	}
+
+    MDL::Matrix<long> mat = load_csv(file);
     long dimension = mat.cols();
     MDL::Vector<long> vec(dimension), vec2(dimension);
     printf("Dimension %ld\n", dimension);
 
     for (size_t i = 0; i < dimension; i++) vec[i] = NTL::RandomBnd(3);
     std::cout << mat.maxEigenValue() << std::endl;
+
     totalTimer.start();
+
     keyTimer.start();
     FHEcontext context(m, p, r);
     buildModChain(context, L);
     FHESecKey  sk(context);
-    sk.GenSecKey(64);	
+    sk.GenSecKey(64);
     addSome1DMatrices(sk);
     FHEPubKey pk = sk;
     auto G = context.alMod.getFactorsOverZZ()[0];
     keyTimer.end();
-    printf("KeyGen %f\n", keyTimer.second());
 
     EncryptedArray ea(context, G);
-        MDL::EncMatrix encMat(pk);
+    MDL::EncMatrix encMat(pk);
     MDL::EncVector encVec(pk), encVec2(pk);
-    TIMING_THIS(
-        encMat.pack(mat, ea);
-        encVec.pack(vec, ea);
-        );
+	MDL::Timer encTimer, evalTimer;
 
-    TIMING_THIS(encVec  = encMat.column_dot(encVec, ea, dimension); );
-    TIMING_THIS(encVec  = encMat.column_dot(encVec, ea, dimension); );
-    TIMING_THIS(encVec  = encMat.column_dot(encVec, ea, dimension); );
-    TIMING_THIS(encVec2 = encMat.column_dot(encVec, ea, dimension); );
+	encTimer.start();
+    encMat.pack(mat, ea);
+    encVec.pack(vec, ea);
+	encTimer.end();
+
+	evalTimer.start();
+    encVec  = encMat.column_dot(encVec, ea, dimension);
+    encVec  = encMat.column_dot(encVec, ea, dimension);
+	if (f < 2) {
+		encVec  = encMat.column_dot(encVec, ea, dimension);
+	}
+    encVec2 = encMat.column_dot(encVec, ea, dimension);
+	evalTimer.end();
 
     {
         MDL::Vector<NTL::ZZX> vec(dimension), vec2(dimension);
-        TIMING_THIS(
-            encVec.unpack(vec, sk, ea);
-            encVec2.unpack(vec2, sk, ea);
-            );
+        encVec.unpack(vec, sk, ea);
+        encVec2.unpack(vec2, sk, ea);
         std::cout << vec2.L2() / vec.L2() << std::endl;
     }
+
     totalTimer.end();
-    printf("total %f\n", totalTimer.second());
+    printf("keygen %f enc %f eval %f total %f\n",
+		   keyTimer.second(), encTimer.second(),
+		   evalTimer.second(), totalTimer.second());
+	return 0;
 }
