e1f91e4917ad10093bbd41593e35b94681529a51
riku <riku@mdl.cs.tsukuba.ac.jp>
2015-08-02 15:03:45 +0900

    new test_mode

--- test/test_mode.cpp
+++ test/test_mode.cpp
@@ -18,20 +18,23 @@ long NR_WORKERS = 1;
 
 MDL::EncVector encrypt(const MDL::Matrix<long> &mat,
                        const FHEPubKey &pk,
-                       const EncryptedArray &ea)
+                       const EncryptedArray &ea,
+					   MDL::Timer &encTimer,
+					   MDL::Timer &evalTimer)
 {
     std::vector<MDL::EncVector> ctxts(mat.rows(), pk);
     std::vector<std::thread> workers;
     std::atomic<long> counter(0);
-    MDL::Timer encTimer, sumTimer;
-    const long ATTRIBUTE = 0;
+    const long ATTRIBUTE = 1;
     encTimer.start();
     for (long wr = 0; wr < NR_WORKERS; wr++) {
         workers.push_back(std::thread([&mat, &counter,
                                        &ctxts, &ea]() {
             long next;
             while ((next = counter.fetch_add(1)) < mat.rows()) {
-                auto encoded = MDL::encoding::indicator(mat[next][ATTRIBUTE], ea);
+			    auto attr = mat[next][ATTRIBUTE];
+				if (attr > 0) attr -= 1;
+                auto encoded = MDL::encoding::indicator(attr, ea);
                 ctxts[next].pack(encoded, ea);
             } }));
     }
@@ -39,17 +42,18 @@ MDL::EncVector encrypt(const MDL::Matrix<long> &mat,
     for (auto &&wr : workers) wr.join();
     encTimer.end();
 
-    sumTimer.start();
+    evalTimer.start();
     for (size_t i = 1; i < mat.rows(); i++) {
         ctxts[0] += ctxts[i];
     }
-    sumTimer.end();
-    printf("enc %f, joined %f\n", encTimer.second(), sumTimer.second());
+    evalTimer.end();
+   // printf("enc %f, joined %f\n", encTimer.second(), sumTimer.second());
     return ctxts[0];
 }
 
 int main(int argc, char *argv[]) {
     long m, p, r, L, N;
+	const long TRIAL = 10;
     ArgMapping argmap;
     argmap.arg("m", m, "m");
     argmap.arg("L", L, "L");
@@ -58,42 +62,45 @@ int main(int argc, char *argv[]) {
     argmap.arg("N", N, "N");
     argmap.parse(argc, argv);
 
-    FHEcontext context(m, p, r);
-    buildModChain(context, L);
-    FHESecKey  sk(context);
-    sk.GenSecKey(64);
-    addSome1DMatrices(sk);
-    auto G = context.alMod.getFactorsOverZZ()[0];
-    EncryptedArray ea(context, G);
-    FHEPubKey pk = sk;
-    printf("Slot %ld\n", ea.size());
+	auto category = load_csv("category.data", N);
+	MDL::Timer encTimer, evalTimer, decTimer, totalTimer;
+	for (long trial = 0; trial < TRIAL; trial++) {
+		totalTimer.start();
+		FHEcontext context(m, p, r);
+		buildModChain(context, L);
+		FHESecKey  sk(context);
+		sk.GenSecKey(64);
+		addSome1DMatrices(sk);
+		auto G = context.alMod.getFactorsOverZZ()[0];
+		EncryptedArray ea(context, G);
+		FHEPubKey pk = sk;
+		if (trial == 0) printf("Slot %ld ", ea.size());
+		N = category.rows();
 
-    MDL::Timer evalTimer, decTimer, totalTimer;
-    auto category = load_csv("category.data", N);
-	N = category.rows();
-	printf("%zd records\n", category.rows());
-    auto joined = encrypt(category, pk, ea);
-    const long domainOfCategory = 20;
-	const long BATCHSZE = domainOfCategory / 10;
-    MDL::Mode::Input input {joined, {0, 0, domainOfCategory}, N};
-	MDL::Matrix<long> mat(domainOfCategory + 1, domainOfCategory + 1);
+		auto joined = encrypt(category, pk, ea, encTimer, evalTimer);
+		const long domainOfCategory = 16;
+		const long BATCHSZE = std::max<long>(domainOfCategory, 1);
+		MDL::Mode::Input input {joined, {0, 0, domainOfCategory}, N};
+		MDL::Matrix<long> mat(domainOfCategory + 1, domainOfCategory + 1);
 
-	totalTimer.start();
-	for (long from = 0; from <= domainOfCategory; from += BATCHSZE) {
-		MDL::Mode::Result::ptr modeResults = nullptr;
-    	evalTimer.start();
-	    input.gt.processFrom = from;
-	    input.gt.processTo = std::min(from + BATCHSZE, domainOfCategory);
-    	MDL::computeMode(input, ea, modeResults);
-    	evalTimer.end();
-    	decTimer.start();
-		MDL::argMode(input, modeResults, sk, ea, mat);
-    	decTimer.end();
+		for (long from = 0; from <= domainOfCategory; from += BATCHSZE) {
+			MDL::Mode::Result::ptr modeResults = nullptr;
+
+			evalTimer.start();
+			input.gt.processFrom = from;
+			input.gt.processTo = std::min(from + BATCHSZE, domainOfCategory);
+			MDL::computeMode(input, ea, modeResults);
+			evalTimer.end();
+
+			decTimer.start();
+			MDL::argMode(input, modeResults, sk, ea, mat);
+			decTimer.end();
+		}
+		totalTimer.end();
+		if (trial == 0) printf(" mode:%ld\n", MDL::argMode(mat));
 	}
-	totalTimer.end();
-	auto evalT = evalTimer.second();
-	auto decT = decTimer.second();
-    printf("The mode is %ld-th category. Eval Dec Other\n %f %f %f\n",
-		   MDL::argMode(mat), evalT, decT, totalTimer.second() - evalT - decT);
-    return 0;
+	printf("%ld %f %f %f %f\n", N,
+		   encTimer.second()/TRIAL, evalTimer.second()/TRIAL,
+		   decTimer.second()/TRIAL, totalTimer.second()/TRIAL);
+	return 0;
 }
