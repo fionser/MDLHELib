c972d3bb6e956db5070d5ae3dbe074ee994b8d87
bob <bob@cs.mdl.tsukuba.ac.jp>
2015-11-19 00:34:04 +0900

    limit package size

--- test/benchmark_network.cpp
+++ test/benchmark_network.cpp
@@ -4,6 +4,16 @@
 #include <string>
 #include "utils/timer.hpp"
 #include "network/network.hpp"
+void make_package(size_t sze, std::vector<size_t> &lens) {
+	const size_t _1KB = 1024;
+	while (sze >= _1KB) {
+		lens.push_back(_1KB);
+		sze -= _1KB;
+	}
+	if (sze > 0)
+		lens.push_back(sze);
+}
+
 void act_client(std::string &addr, size_t sze) {
 	int sock = nn_socket(AF_SP, NN_REQ);
 	std::string host = "tcp://" + addr + ":12345";
@@ -13,21 +23,29 @@ void act_client(std::string &addr, size_t sze) {
             return;
 	}
 
+	std::vector<size_t> lens;
+	make_package(sze, lens);
 	std::vector<void *> data;	
-	std::vector<int> _data(sze / sizeof(int));
-	data.push_back((void *)_data.data());
-	std::vector<size_t> lens = {sze};
+	for (auto len : lens)
+		data.push_back((void *)new char[len]);
+	
 	struct nn_msghdr nn_hdr;
 	MDL::net::make_nn_header(&nn_hdr, data, lens);
 	MDL::Timer timer;
 	nn_send(sock, NULL, 0, 0);
 	nn_recv(sock, NULL, 0, 0);
+	printf("going to send %zd!\n", lens.size());
 	timer.start();
 	nn_sendmsg(sock, &nn_hdr, 0);
 	nn_recv(sock, NULL, 0, 0);
 	timer.end();
 	printf("send %f\n", timer.second());
+	nn_close(sock);
 	MDL::net::free_header(&nn_hdr, false);
+	for (auto p : data) {
+		char *pp = (char *)p;
+		delete []pp;
+	}
 }
 
 void act_server(size_t sze) {
@@ -39,7 +57,8 @@ void act_server(size_t sze) {
 	}
 	nn_recv(sock, NULL, 0, 0);
 	nn_send(sock, NULL, 0, 0);
-	std::vector<size_t> lens = {sze};	
+	std::vector<size_t> lens;
+	make_package(sze, lens);
 	struct nn_msghdr nn_hdr;
 	MDL::Timer timer;
 	timer.start();
@@ -49,6 +68,7 @@ void act_server(size_t sze) {
 	nn_send(sock, NULL, 0, 0);
 	MDL::net::free_header(&nn_hdr, true);
 	printf("receive %f\n", timer.second());
+	nn_close(sock);
 }
 
 int main(int argc, char *argv[]) {
