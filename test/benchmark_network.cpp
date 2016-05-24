feef249d405caecb639909817cab2456f1ee3021
bob <bob@cs.mdl.tsukuba.ac.jp>
2016-05-24 15:01:51 +0900

    benchmark_network

--- test/benchmark_network.cpp
+++ test/benchmark_network.cpp
@@ -3,8 +3,27 @@
 #include <unistd.h>
 #include <string>
 #include <stdio.h>
+#include <cmath>
 #include "utils/timer.hpp"
 #include "network/network.hpp"
+std::pair<double, double> mean_std(const std::vector<double> &v) {
+	double m = 0;
+	for (auto vv : v) m += vv;
+	m /= v.size();
+
+	double s = 0;
+	for (auto vv : v) {
+		s += (vv - m) * (vv - m);
+	}
+
+	if (v.size() > 1)
+		s = std::sqrt(s / (v.size() - 1));
+	else
+		s = 0.0;
+
+	return std::make_pair(m, s);
+}
+
 void make_package(size_t sze, std::vector<size_t> &lens) {
 	const size_t _1KB = 1024;
 	while (sze >= _1KB) {
@@ -37,17 +56,21 @@ void act_client(std::string &addr, size_t sze) {
 	std::vector<void *> data;	
 	for (auto len : lens)
 		data.push_back((void *)new char[len]);
-	
+		
+	std::vector<double> times;
 	MDL::Timer timer;
 	nn_send(sock, NULL, 0, 0);
 	nn_recv(sock, NULL, 0, 0);
-
-	timer.start();
-	MDL::net::send_all(sock, data, lens);
-	timer.end();
-	printf("send %f\n", timer.second());
+   	for (int i = 0; i < 10; i++) {		
+		timer.start();
+		MDL::net::send_all(sock, data, lens);
+		timer.end();	
+		times.push_back(timer.second());
+		//printf("send %zd %f\n", sze, timer.second());
+        }
 	nn_close(sock);
-
+	auto ms = mean_std(times);
+ 	printf("send %zd %f +- %f\n", sze, ms.first, ms.second);	
 	for (auto p : data) {
 		char *pp = (char *)p;
 		delete []pp;
@@ -62,18 +85,22 @@ void act_server(size_t sze) {
             return;
 	}
 
-	nn_recv(sock, NULL, 0, 0);
-	nn_send(sock, NULL, 0, 0);
-
 	std::vector<size_t> lens;
 	make_package(sze, lens);
 
-	MDL::Timer timer;
-	timer.start();
-	MDL::net::receive_all(sock, lens);
-	timer.end();
+	nn_recv(sock, NULL, 0, 0);
+	nn_send(sock, NULL, 0, 0);
 
-	printf("receive %f\n", timer.second());
+	MDL::Timer timer;
+	std::vector<double> times;
+	for (int i = 0; i < 10; i++) {
+		timer.start();
+		MDL::net::receive_all(sock, lens);
+		timer.end();
+		times.push_back(timer.second());
+	}
+	auto ms = mean_std(times);
+ 	printf("receive %zd %f +- %f\n", sze, ms.first, ms.second);	
 	nn_close(sock);
 }
 
